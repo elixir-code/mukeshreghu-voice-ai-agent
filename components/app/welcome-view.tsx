138d3da9f50f29dc5b8ed13c58b83f7efb5e825b
R Mukesh <ced15i002@iiitdm.ac.in>
2025-12-13 12:04:24 +0000

    Customize UI from default
    - Remove livekit logo from top right
    - Modify footer to point to personal homepage

--- components/app/welcome-view.tsx
+++ components/app/welcome-view.tsx
@@ -34,7 +34,7 @@ export const WelcomeView = ({
         <WelcomeImage />
 
         <p className="text-foreground max-w-prose pt-1 leading-6 font-medium">
-          Chat live with your voice AI agent
+          Chat live with my voice AI agent
         </p>
 
         <Button variant="primary" size="lg" onClick={onStartCall} className="mt-6 w-64 font-mono">
@@ -43,18 +43,16 @@ export const WelcomeView = ({
       </section>
 
       <div className="fixed bottom-5 left-0 flex w-full items-center justify-center">
-        <p className="text-muted-foreground max-w-prose pt-1 text-xs leading-5 font-normal text-pretty md:text-sm">
-          Need help getting set up? Check out the{' '}
+        <span className="text-foreground font-mono text-xs font-bold tracking-wider uppercase">
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://docs.livekit.io/agents/start/voice-ai/"
-            className="underline"
+            href="https://mukeshreghu.com"
+            className="underline underline-offset-4"
           >
-            Voice AI quickstart
+            mukeshreghu.com
           </a>
-          .
-        </p>
+        </span>
       </div>
     </div>
   );
